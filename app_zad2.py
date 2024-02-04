62619990f5b82073b6d2e2700b3b807fe784b642
Mikolaj Gawronski <mikgaw001@pbs.edu.pl>
2024-02-04 20:50:44 +0100

    poprawka

--- app_zad2.py
+++ app_zad2.py
@@ -1,4 +1,29 @@
 def add_numbers(a, b):
+    return a+b
+
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+def multiply_numbers(a, b):
+    return a * b
+
+
+def divide_numbers(a, b):
+    if b == 0:
+        raise ValueError("Dzielenie przez zero jest niedozwolone.")
+    return a / b
+
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 5
+    print(f"Dodawanie: {add_numbers(num1, num2)}")
+    print(f"Odejmowanie: {subtract_numbers(num1, num2)}")
+    print(f"Mnożenie: {multiply_numbers(num1, num2)}")
+    print(f"Dzielenie: {divide_numbers(num1, num2)}")
+def add_numbers(a, b):
  return a+b
 def subtract_numbers(a , b):
  return a - b
@@ -15,3 +40,31 @@ if _name_ == "_main_":
  print(f"Odejmowanie: {subtract_numbers(num1,num2)}")
  print(f"Mnożenie: {multiply_numbers( num1, num2)}")
  print(f"Dzielenie: {divide_numbers(num1 , num2)}")
+Dzisiaj o 20:48
+20:48
+Wysłano
+def add_numbers(a, b):
+    return a+b
+
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+def multiply_numbers(a, b):
+    return a * b
+
+
+def divide_numbers(a, b):
+    if (b == 0):
+        raise ValueError("Dzielenie przez zero jest niedozwolone.")
+    return a / b
+
+
+if _name_ == "_main_":
+    num1 = 10
+    num2 = 5
+    print(f"Dodawanie: {add_numbers(num1, num2)}")
+    print(f"Odejmowanie: {subtract_numbers(num1, num2)}")
+    print(f"Mnożenie: {multiply_numbers(num1, num2)}")
+    print(f"Dzielenie: {divide_numbers(num1, num2)}")
